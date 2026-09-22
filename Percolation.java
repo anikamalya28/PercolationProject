9b63dd47a52d378b4dd27bdb7419665f1d0c8027
anikamalya28 <amaly28@jesuitmail.org>
2026-09-22 13:49:09 -0700

    bob

--- Percolation.java
+++ Percolation.java
@@ -1,9 +1,45 @@
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    private boolean[] grid;
+    private WeightedQuickUnionUF uf;
+    private int n;
 
+    // creates n-by-n grid, with all sites initially blocked
+    public Percolation(int n) throws IllegalArgumentException {
+        if (n<=0) {throw new IllegalArgumentException("error");}
 
-    public static void main(String[] args) {
+        grid = new boolean[n*n+2];
+        //false is blocked
 
+        uf = new WeightedQuickUnionUF(n*n+2);
     }
+
+    // opens the site (row, col) if it is not open already
+    public void open(int row, int col) throws IllegalArgumentException {
+        if (row<=0 || col<=0 || row > n || col > n) {throw new IllegalArgumentException("error");}
+        grid[n*(row-1)+col] = true;
+    }
+
+    // is the site (row, col) open?
+    public boolean isOpen(int row, int col) throws IllegalArgumentException {
+        if (row<=0 || col<=0 || row > n || col > n) {throw new IllegalArgumentException("error");}
+        return grid[n*(row-1)+col];
+    }
+/*
+    // is the site (row, col) full?
+    public boolean isFull(int row, int col)
+
+    // returns the number of open sites
+    public int numberOfOpenSites()
+
+    // does the system percolate?
+    public boolean percolates()
+
+    // test client (optional)
+    public static void main(String[] args)
+
+ */
 }
+
+
